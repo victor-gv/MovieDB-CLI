f3fd748f28eeb4e07199d87af7d4d207adfd93fb
juliomc23 <julito2302mc@gmail.com>
2022-09-05 16:13:25 +0200

    getting page number

--- moviedb.js
+++ moviedb.js
@@ -1,56 +1,44 @@
-const https = require('https');
+const https = require("https");
 
-require('dotenv').config({ path: `${__dirname}/src/.env` })
+require("dotenv").config({ path: `${__dirname}/src/.env` });
 
 const apiKey = process.env.API_KEY;
 
-const { program } = require('commander');
+const { program } = require("commander");
 
 program
-	//.command('get-persons')
-	.description('Make a network request to fetch the most popular persons')
-	.action(getPersons())
-	
-	//Options
-  .option('-p, --popular', 'Fetch the popular persons')
-  .option('--page', 'The page of persons data results to fetch')
-  .action(function(id){
-		https.get(`https://api.themoviedb.org/3/person/popular?api_key=${apiKey}&page=${id}`, (resp) => {
-			let data = '';
-	
-			// A chunk of data has been received.
-			resp.on('data', (chunk) => {
-				data += chunk;
-			});
-		
-			// The whole response has been received. Print out the result.
-			resp.on('end', () => {
-				console.log(JSON.parse(data));
-			});
-		
-		}).on("error", (err) => {
-			console.log("Error: " + err.message);
-		});
-	})
+  .command("get-persons")
+  .description("Make a network request to fetch the most popular persons")
+  .option("-p, --popular", "Fetch the popular persons")
+  .option("--page <type>", "The page of persons data results to fetch")
+  .action(function (props) {
+    // console.log(props);
+    const { page } = props;
+    // const pageType = Object.keys(props).find((key) => {
+    //   if (key === "popular") {
+    //     return key;
+    //   }
+    // });
 
+    // console.log(pageType);
+    https
+      .get(
+        `https://api.themoviedb.org/3/person/popular?api_key=${apiKey}&page=${page}`,
+        (resp) => {
+          let data = "";
+          // A chunk of data has been received.
+          resp.on("data", (chunk) => {
+            data += chunk;
+          });
+          // The whole response has been received. Print out the result.
+          resp.on("end", () => {
+            console.log(JSON.parse(data));
+          });
+        }
+      )
+      .on("error", (err) => {
+        console.log("Error: " + err.message);
+      });
+  });
 
-  
-function getPersons(id = 1) {
-	https.get(`https://api.themoviedb.org/3/person/popular?api_key=${apiKey}&page=${id}`, (resp) => {
-		let data = '';
-
-		// A chunk of data has been received.
-		resp.on('data', (chunk) => {
-			data += chunk;
-		});
-	
-		// The whole response has been received. Print out the result.
-		resp.on('end', () => {
-			console.log(JSON.parse(data));
-		});
-	
-	}).on("error", (err) => {
-		console.log("Error: " + err.message);
-	});
-}
-
+program.parse(process.argv);
